68e24cc5f8ee764af66ed422706f5bbe21d48207
Robert Olson <olson@mcs.anl.gov>
2015-07-08 16:41:34 -0500

    many widgetification fixes

--- seed/seed-compare-widget.js
+++ seed/seed-compare-widget.js
@@ -3,7 +3,9 @@ define([
     'dojo/parser',
     'dojo/ready',
     'dojo/dom-style',
-    'dijit/_WidgetBase',
+    'dojo/query',
+    'dijit/_Widget',
+    'dijit/_AttachMixin',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!seed/seed-compare-template.html',
@@ -15,18 +17,21 @@ define([
     'dijit/dijit',
     'dijit/form/TextBox',
     'dijit/form/Button',
+    'dijit/form/RadioButton',
     'dijit/form/CheckBox',
     'dijit/form/NumberTextBox'
-], function(declare, parser, ready, domStyle, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template,
+], function(declare, parser, ready, domStyle, query, _Widget, _AttachMixin, 
+	    _TemplatedMixin, _WidgetsInTemplateMixin, template,
 	    CompareRegions, SEEDClient) {
-    declare("seed.CompareRegionsWidget", [_WidgetBase, _TemplatedMixin], {
+    declare("seed.CompareRegionsWidget", [_Widget, _TemplatedMixin, _WidgetsInTemplateMixin], {
+
 	target_div: null,
 	container: null,
 	palette: null,
 	service_url: null,
 	service: null,
-	width: null,
-	n_genomes: null,
+	n_regions: null,
+	region_size: null,
 	color_mode: 'blast',
 	focus_feature: null,
 	compare_regions: null,
@@ -35,6 +40,14 @@ define([
 
 	postCreate: function() {
 	    window.console.log("post create");
+	    this.inherited(arguments);
+	    // this.button_left_2.connect("onClick", function() { window.console.log("left2"); });
+	},
+	
+	startup: function() {
+
+	    this.inherited(arguments);
+
 	    this.service = new SEEDClient(this.service_url);
 	    this.compare_regions = new CompareRegions(this.canvas, this.service);
 
@@ -44,9 +57,6 @@ define([
 		this.render();
 	    }.bind(this));
 
-	},
-
-	startup: function() {
 	    this.service.get_palette('compare_region', function(palette) {
 		this.compare_regions.set_palette(palette);
 		this.render();
@@ -54,15 +64,28 @@ define([
 	},
 
 	render: function() {
-	    this.service.compare_regions_for_peg(this.focus_feature, this.width,
-							 this.n_genomes, this.color_mode,
-							 function(data) {
-							     this.compare_regions.set_data(data);
-							     this.compare_regions.render();
-							 }.bind(this));
-	},
 
-	redraw: function(evt) {
+	    var coloring_algo = query('input[type=radio]:checked', this.coloring_algo_group.domNode)[0].value;
+	    var select_pinned_pegs = query('input[type=radio]:checked', this.select_pinned_pegs_group.domNode)[0].value;
+	    var genome_selection = query('input[type=radio]:checked', this.genome_selection_group.domNode)[0].value;
+	    var genome_sort = query('input[type=radio]:checked', this.genome_sort_group.domNode)[0].value;
+	    
+	    window.console.log(coloring_algo,
+			       select_pinned_pegs, genome_selection, genome_sort,
+			       this.region_size.get('value'),
+			       this.n_regions.get('value'),
+			       this.pin_cutoff.get('value'),
+			       this.coloring_cutoff.get('value')
+			      );
+
+	    this.service.compare_regions_for_peg(this.focus_feature,
+						 this.region_size.get('value'),
+						 this.n_regions.get('value'),
+						 coloring_algo,
+						 function(data) {
+						     this.compare_regions.set_data(data);
+						     this.compare_regions.render();
+						 }.bind(this));
 	},
 
 	left_2: function(evt) {
@@ -81,12 +104,8 @@ define([
 	    window.console.log("right_1");
 	},
 
-	display_regular: function() {
-	    domStyle.set(this.advanced_settings, 'display', 'none');
-	},
-	
-	display_advanced: function() {
-	    domStyle.set(this.advanced_settings, 'display', 'block');
+	display_regular: function(val) {
+	    domStyle.set(this.advanced_settings, 'display', val ? 'none' : 'block');
 	},
 	
 	_getFocus_featureAttr: function() {
@@ -102,7 +121,7 @@ define([
     });
 
     ready(function() {
-	window.console.log("ready!");
-	parser.parse();
+	window.console.log("widget ready!");
+
     });
 });
